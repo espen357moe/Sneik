503884a5e61075fa7ab32b16d5624271048f9cb0
unknown <012408@S-HVE8470.hbv.no>
2015-02-03 14:39:25 +0100

    La til initialoppsett

--- Sneik/Form1.cs
+++ Sneik/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,17 +13,45 @@ namespace Sneik
 {
     public partial class Form1 : Form
     {
-
+        
         ArrayList bodySegments;
         Point treasure;
+        Char direction = "L";
+        Random r;
+        int score;
+        Font fnt;
+
+        private Color GetRandomColor()
+        {
+            return Color.FromArgb(r.Next(0, 255), r.Next(0,255),r.Next(0,255));
+        }
 
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void configuration()
+        {
+            score = 0;
+            bodySegments = new ArrayList();
+            bodySegments.Add(new Point(20, 10));
+            bodySegments.Add(new Point(21, 10));
+            bodySegments.Add(new Point(22, 10));
+            bodySegments.Add(new Point(23, 10));
+            bodySegments.Add(new Point(24, 10));
+            r = new Random();
+            fnt = new Font("Verdana", 12, FontStyle.Bold);
+            treasure.X = r.Next(0, 39);
+            treasure.Y = r.next(0, 19);
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            Size s = new System.Drawing.Size(400, 200);
+            this.ClientSize = s;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            configuration();
 
         }
     }
