ae9dce1be4fadd85d0f6d719f589efeff1f37fc3
Espen Moe <espen357moe@gmail.com>
2015-05-10 18:27:58 +0200

    La til timer og håndterer for tastetrykk

--- Sneik/Form1.cs
+++ Sneik/Form1.cs
@@ -27,15 +27,18 @@ namespace Sneik
 
         private void configuration()
         {
+            fnt = new Font("Verdana", 12, FontStyle.Bold);
             score = 0;
+
             bodySegments = new ArrayList();
             bodySegments.Add(new Point(20, 10));
             bodySegments.Add(new Point(21, 10));
             bodySegments.Add(new Point(22, 10));
             bodySegments.Add(new Point(23, 10));
             bodySegments.Add(new Point(24, 10));
+            
             r = new Random();
-            fnt = new Font("Verdana", 12, FontStyle.Bold);
+            
             treasure.X = r.Next(0, 39);
             treasure.Y = r.Next(0, 19);
         }
@@ -46,6 +49,7 @@ namespace Sneik
             this.ClientSize = s;
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
             configuration();
+            timer1.Enabled = true;
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
@@ -53,11 +57,13 @@ namespace Sneik
             Graphics g;
             g = e.Graphics;
             SolidBrush b = new SolidBrush(Color.Green);
+            
             for (int i = 0; i < bodySegments.Count; i++)
             {
                 Point bodySegment = (Point)bodySegments[i];
                 g.FillEllipse(b, bodySegment.X * 10, bodySegment.Y * 10, 10, 10);
             }
+            
             b.Color = Color.FromArgb(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255));
             g.FillRectangle(b, treasure.X * 10, treasure.Y * 10, 10, 10);
             b.Color = Color.DarkGray;
@@ -67,7 +73,43 @@ namespace Sneik
         private void timer1_Tick(object sender, EventArgs e)
         {
             Point p = (Point)bodySegments[0];
-            //mer her
+            switch (direction)
+            {
+                case 'L' : bodySegments.Insert(0, new Point(p.X - 1, p.Y));
+                    break;
+                case 'R' : bodySegments.Insert(0, new Point(p.X + 1, p.Y));
+                    break;
+                case 'U' : bodySegments.Insert(0, new Point(p.X, p.Y - 1));
+                    break;
+                case 'D' : bodySegments.Insert(0, new Point(p.X, p.Y + 1));
+                    break;
+            }
+
+            bodySegments.RemoveAt(bodySegments.Count - 1);
+            this.Refresh();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down & direction != 'U')
+            {
+                direction = 'D';
+            }
+
+            if (e.KeyCode == Keys.Up & direction != 'D')
+            {
+                direction = 'U';
+            }
+
+            if (e.KeyCode == Keys.Left & direction != 'R')
+            {
+                direction = 'L';
+            }
+
+            if (e.KeyCode == Keys.Right & direction != 'L')
+            {
+                direction = 'R';
+            }
         }
     }
 }
